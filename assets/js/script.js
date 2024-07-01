e673c744f475ec59f6449270517cc373541448ba
alessandrovarc-git <alessandro.varchetta1996@gmail.com-git>
2024-07-02 00:19:33 +0200

    stelle seconda parte

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,6 +1,15 @@
-const star = '<img class="disable-star" src="assets/img/star.svg"/>';
+//const star = '<img class="disable-star" src="assets/img/star.svg"/>';
 const starsContainer = document.getElementById("feedback-stars-container");
 
 for (let i = 0; i < 10; i++) {
-    starsContainer.innerHTML += star;
+    starsContainer.innerHTML += `<img id="star-${i}" onmouseover="onStarHover(event)" onmouseleave="onStarLeave(event)" class="disable-star" src="assets/img/star.svg"/>`;
 }
+const onStarHover = (event) => {
+    const img = document.getElementById(event.target.id);
+    img.classList.add('enabled');
+};
+
+const onStarLeave = (event) => {
+    const img = document.getElementById(event.target.id);
+    img.classList.remove('enabled');
+};
