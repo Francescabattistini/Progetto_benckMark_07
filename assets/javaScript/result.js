5a36e7ffb9e959bd709c92cbecc6408e97fe1f52
Francescabattistini <166622198+Francescabattistini@users.noreply.github.com>
2024-07-01 15:28:11 +0200

    thin

---
+++ assets/javaScript/result.js
@@ -0,0 +1,17 @@
+//recuperare risposte sbagliate
+
+// creo una funzione che itera le risposte
+// creo una condizione se le risposte sono sbagliate stampami nell apagina Wrog
+// dammi la percentuale delle risposte sbagliate
+// segnami quante sbagliate sul totale risposte
+
+//recuperare risposte giuste
+
+// creo una funzione che itera le risposte
+// creo una condizione se le risposte sono giuste stampami nell apagina Correct
+// dammi la percentuale delle risposte giuste
+// segnami quante giuste sul totale risposte
+
+//creo il grafico
+
+// creo il bottone
